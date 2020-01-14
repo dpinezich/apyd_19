c32cf85d9d899576457a932e77341a2cf380ccf0
dpinezich <david.pinezich@gmail.com>
2020-01-14 23:21:23 +0100

    added first glimpse into session 3

--- exercises/amazon_exercise/visualization.py
+++ exercises/amazon_exercise/visualization.py
@@ -24,31 +24,44 @@ def create_wordcloud(processed_text, filename):
     pyplot.savefig(filename)
 
 
-def create_review_length_boxplot(reviews, filename):
+def create_review_length_boxplot(reviews, filename, box_mode='tukey'):
     """
-     Create a graph that shows a boxplot visualizes the review length for each score (1-star, 2-star, 3-star, 4-star, 5-star)
-     Documentation about boxplot charts: http://www.pygal.org/en/latest/documentation/types/box.html
-    :param reviews: list with review dictionaries
-    :param filename: name of the saved file
-    :return:
+     Creates a boxplot chart with five entries that show the distribution of the review lengths for all possible scores
+     Reviews longer than 1500 words are ignored in this example
+     Chart documentation: http://www.pygal.org/en/latest/documentation/types/box.html
+    :param reviews: list of review dictionaries
+    :param filename: string
+    :param box_mode: type of boxchart, e.g. None, 'tukey', 'stdv'
     """
     one_star_review_lengths = []
     two_star_review_lengths = []
     three_star_review_lengths = []
     four_star_review_lengths = []
     five_star_review_lengths = []
+    for review in reviews:
+        score = int(review['review/score'])
+        review_length = len(review['review/text'].split(' '))
+        if review_length > 1500:  # ignore some outliers
+            continue
+        if score == 1:
+            one_star_review_lengths.append(review_length)
+        elif score == 2:
+            two_star_review_lengths.append(review_length)
+        elif score == 3:
+            three_star_review_lengths.append(review_length)
+        elif score == 4:
+            four_star_review_lengths.append(review_length)
+        elif score == 5:
+            five_star_review_lengths.append(review_length)
 
-    # for review in reviews:
-    # ...
-    # ...
-
-    box_plot = pygal.Box(box_mode='tukey')
-
-    # Implementation...
-    # ...
-    # ...
-    # ...
-
+    box_plot = pygal.Box(box_mode=box_mode)
+    box_plot.title = 'Review Lengths by Review Score'
+    box_plot.add('1 Star Reviews', one_star_review_lengths)
+    box_plot.add('2 Star Reviews', two_star_review_lengths)
+    box_plot.add('3 Star Reviews', three_star_review_lengths)
+    box_plot.add('4 Star Reviews', four_star_review_lengths)
+    box_plot.add('5 Star Reviews', five_star_review_lengths)
+    box_plot.x_labels = ['1 Star', '2 Stars', '3 Stars', '4 Stars', '5 Stars']
     box_plot.render_to_file(filename)
 
 
